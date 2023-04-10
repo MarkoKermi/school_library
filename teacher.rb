6f5e20b5eb27c8ba2c6bdab451b73c8437ec9826
Marko Kermiciev <kermicievmarko@gmail.com>
2023-04-10 14:59:04 +0200

    creating classes

---
+++ teacher.rb
@@ -0,0 +1,12 @@
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(age, name = 'Unknown', parent_permission = true, specialization)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
