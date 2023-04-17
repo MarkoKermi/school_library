5f3acfb67ed547acb3df9a58d194991453a197a9
Marko Kermiciev <kermicievmarko@gmail.com>
2023-04-17 15:21:35 +0200

    refactoring the code

--- app.rb
+++ app.rb
@@ -15,17 +15,6 @@ class SchoolApp
     puts 'Welcome to School Library App!'
   end
 
-  def menu
-    puts 'Please choose an option by entering a number: '
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
-
   def list_of_books
     if @books.empty?
       puts 'There is no books in the list'
@@ -122,35 +111,4 @@ class SchoolApp
       puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}" if rental.person.id == id
     end
   end
-
-  # rubocop:disable Metrics/MethodLength
-  # rubocop:disable Metrics/CyclomaticComplexity
-  def run
-    welcome
-
-    loop do
-      menu
-      option = gets.chomp
-
-      case option
-      when '1'
-        list_of_books
-      when '2'
-        list_of_people
-      when '3'
-        add_person_info
-      when '4'
-        book_creation
-      when '5'
-        rental_creation
-      when '6'
-        list_of_rentals_person_id
-      when '7', 'q', 'quit'
-        break
-      end
-    end
-    puts 'Thank you for using this app!'
-  end
-  # rubocop:enable Metrics/MethodLength
-  # rubocop:enable Metrics/CyclomaticComplexity
 end
